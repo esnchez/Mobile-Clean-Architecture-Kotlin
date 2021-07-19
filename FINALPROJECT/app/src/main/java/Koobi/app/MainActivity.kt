29de13d715a91c620573019d3de323c3cbb18919
Eric <sanchez.eric@epitech.eu>
2021-07-19 13:04:50 +0200

    Adding the menu bar and setting up the functionality -part1

--- FINALPROJECT/app/src/main/java/Koobi/app/MainActivity.kt
+++ FINALPROJECT/app/src/main/java/Koobi/app/MainActivity.kt
@@ -1,14 +1,37 @@
 package Koobi.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+/*
+    var bottomNavigationView: BottomNavigationView? = null
+*/
+
     override fun onCreate(savedInstanceState:  Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavigation();
 
+
+    }
+
+    private fun setUpNavigation() {
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
+
+        val naveController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.Navigation_menu)
+        bottomNavigationView.setupWithNavController(naveController);
+
     }
 }
+
+
