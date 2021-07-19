5e60d235db54acf87d35994d34334dcd73a83d6a
FrancescMadrona <madrona.francesc@epitech.eu>
2021-07-19 17:08:51 +0200

    setting up botton nav bar

--- FINALPROJECT/app/src/main/java/Koobi/app/MainActivity.kt
+++ FINALPROJECT/app/src/main/java/Koobi/app/MainActivity.kt
@@ -1,38 +1,47 @@
 package Koobi.app
 
+import Koobi.app.Navigation.CustomNavigator
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.Navigation
+import androidx.navigation.*
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
+
 class MainActivity : AppCompatActivity() {
 /*
     var bottomNavigationView: BottomNavigationView? = null
 */
-
     override fun onCreate(savedInstanceState:  Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpNavigation()
+        setUpCustomNavigation();
 
 
     }
 
-    private fun setUpNavigation() {
+    private fun setUpCustomNavigation() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
-        val naveController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.Navigation_menu)
-        bottomNavigationView.setupWithNavController(naveController)
 
+        // setup custom navigator
+        val navigator = CustomNavigator(this, navHostFragment.childFragmentManager, R.id.nav_host_fragment)
+        navController.navigatorProvider.addNavigator(navigator);
+
+
+        // set navigation graph
+        navController.setGraph(R.navigation.nav_graph)
+
+        //setupActionBarWithNavController(navController)
+        //val naveController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.Navigation_menu)
+        bottomNavigationView.setupWithNavController(navController);
     }
 }
 
--- FINALPROJECT/app/src/main/java/Koobi/app/commandFragment.kt
+++ FINALPROJECT/app/src/main/java/Koobi/app/commandFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class commandFragment : Fragment() {
-    //I'm on the branch Fran
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
