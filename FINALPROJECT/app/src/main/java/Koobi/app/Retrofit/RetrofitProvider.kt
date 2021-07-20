506e9f80e7f63766f285a717876acfaac9cee1e7
FrancescMadrona <madrona.francesc@epitech.eu>
2021-07-20 12:54:57 +0200

    api call working but not with injection

--- FINALPROJECT/app/src/main/java/Koobi/app/Retrofit/RetrofitProvider.kt
+++ FINALPROJECT/app/src/main/java/Koobi/app/Retrofit/RetrofitProvider.kt
@@ -3,9 +3,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-class RetrofitProvider {
+ class RetrofitProvider {
 
-    private fun getRetrofit(): Retrofit {
+    public fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://api.chucknorris.io/jokes/")
             .addConverterFactory(GsonConverterFactory.create())
--- FINALPROJECT/app/src/main/java/Koobi/app/testFragment.kt
+++ FINALPROJECT/app/src/main/java/Koobi/app/testFragment.kt
@@ -1,28 +1,49 @@
 package Koobi.app
 
+import Koobi.app.Retrofit.ApiService
 import Koobi.app.Retrofit.JokeResponse
 import Koobi.app.Retrofit.RetrofitProvider
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class testFragment : Fragment() {
+class testFragment : Fragment()  {
+    private lateinit var retrofit : RetrofitProvider
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.test_fragment, container, false)
-
+        val view = inflater.inflate(R.layout.test_fragment, container, false)
+        val textView : TextView = view.findViewById(R.id.textChanging)
+        fetchJoke(textView)
+        return view
     }
 
 
-    fun fetchJoke() : String {
-        var call = RetrofitProvider
+
+    private fun fetchJoke(textView: TextView) {
+        retrofit = RetrofitProvider()
+        val call = retrofit.getRetrofit().create(ApiService::class.java).getRandomJoke()
+        call.enqueue(object : Callback<JokeResponse> {
+            override fun onFailure(call: Call<JokeResponse>?, t: Throwable?) {
+                Log.v("retrofit", "call failed")
+
+            }
+
+            override fun onResponse(call: Call<JokeResponse>?, response: Response<JokeResponse>?) {
+                Log.v("retrofit", "call success --> " + (response!!.body()!!.Joke))
+                textView.text = response.body()!!.Joke
+            }
+        })
     }
-
-
 }
