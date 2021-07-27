ea912c20ab61b6dd27fd2fb5618373dc1f0d75a0
Eric <sanchez.eric@epitech.eu>
2021-07-27 14:23:44 +0200

    setting up the fragments to fit the app logic

--- FINALPROJECT/presenter/src/main/java/Koobi/presenter/view/fragments/CommandFragment.kt
+++ FINALPROJECT/presenter/src/main/java/Koobi/presenter/view/fragments/CommandFragment.kt
@@ -1,18 +1,13 @@
 package Koobi.presenter.view.fragments
 
 import Koobi.presenter.R
+import Koobi.presenter.view.base.BaseFragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class CommandFragment : Fragment(){
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.command_fragment, container, false)
-    }
+class CommandFragment : BaseFragment(){
+    override fun layoutRes() = R.layout.command_fragment
 }
